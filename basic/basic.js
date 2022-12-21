4ac77a79ed370224186089172f78a79f0e725a43
Courdevelops <courdevelops@gmail.com>
2022-12-21 15:02:14 -0500

    if statements added to output

--- basic/basic.js
+++ basic/basic.js
@@ -11,9 +11,11 @@ window.addEventListener('load', () =>{
     let buttons = document.getElementById("buttons");
     console.log("The buttons div:", buttons);
     // button 1
-    // let first = document.getElementById("button1");
+    let first = document.getElementById("one");
+    console.log("The button1 div:", first);
     // add event listener to button 1
-    buttons.addEventListener('click', (e) => {
+    // You have to do each button for add event listner
+    first.addEventListener('click', (e) => {
         // if button 1
         if(e.target && e.target.id === "one"){
             // extract the value from button 1
@@ -21,20 +23,40 @@ window.addEventListener('load', () =>{
             // transfer to the same localStorage unit
             localStorage.setItem("BUTTON1", Uno);
         }
+    // button 2
+    let second = document.getElementById("two");
+    console.log("The button2:", second);
+    // add event listener to button 2
+    second.addEventListener('click', (e) => {
         if(e.target && e.target.id === "two"){
             // extract the value from button 2
             let Dos = e.target.value;
             // transfer to the same localStorage unit
             localStorage.setItem("BUTTON2", Dos);
         }
+    });
+    // button 3
+    let third = document.getElementById("three");
+    console.log("The button3:", third);
+    // add event listener to button 3
+    third.addEventListener('click', (e) => {
         if(e.target && e.target.id === "three"){
             let Tres = e.target.value;
             localStorage.setItem("BUTTON3", Tres);
         }
+    });
+    // button 4
+    let fourth = document.getElementById("four");
+    console.log("The button4:", fourth);
+    // add event listener to button 4
+    fourth.addEventListener('click', (e) => {
+        // add event listener to button 4
         if(e.target && e.target.id === "four"){
             let Quatro = e.target.value;
             localStorage.setItem("BUTTON4", Quatro);
         }
-    // localStorage.setItem("BUTTONS", buttons);
+    })
+    localStorage.setItem("BUTTONS", buttons);
+    console.log("BUTTONS");
     });
 });
--- basic/output.js
+++ basic/output.js
@@ -9,19 +9,31 @@ window.addEventListener('load', () => {
     var button1 = localStorage.getItem("BUTTON1");
     console.log(button1);
     document.getElementById("show").innerHTML = button1;
+    if(button1){
+        document.getElementById("show").innerHTML = button1;
+        console.log("button One Pressed");
+    }
     
     var button2 = localStorage.getItem("BUTTON2");
     console.log(button2);
-    document.getElementById("show").innerHTML = button2;
-    
+    if(button2){
+        document.getElementById("show").innerHTML = button2;
+        console.log("button Two Pressed");
+    }
 
     var button3 = localStorage.getItem("BUTTON3");
     console.log(button3);
-    document.getElementById("show").innerHTML = button3;
+    if(button3){
+        document.getElementById("show").innerHTML = button3;
+        console.log("button Three Pressed");
+    }
 
     var button4 = localStorage.getItem("BUTTON4");
     console.log(button4);
-    document.getElementById("show").innerHTML = button4;
+    if(button4){
+        document.getElementById("show").innerHTML = button4;
+        console.log("button Four Pressed");
+    }
 });
 
 
